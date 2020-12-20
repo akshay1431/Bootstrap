a32fe41605b6c25f9b54cbf5caa9dc03454e00bf
akshay1431 <48290918+akshay1431@users.noreply.github.com>
2020-12-20 08:58:02 +0530

    import js

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("Hello Talk Banana")
+
+
+//input
+var username = prompt("enter username")
+
+//processing 
+var welcomeMessage = "This script works "+username;
+
+//output
+alert(welcomeMessage);
