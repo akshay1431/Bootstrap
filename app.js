ed4b7d2a0c0ba3c5ed8819826aae583f946f67fd
akshay1431 <48290918+akshay1431@users.noreply.github.com>
2020-12-21 12:46:20 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-// console.log("Hello Talk Banana")
+// // console.log("Hello Talk Banana")
 
 
-//input
-var username = prompt("enter username")
+// //input
+// var username = prompt("enter username")
 
-//processing 
-var welcomeMessage = "This script works "+username;
+// //processing 
+// var welcomeMessage = "This script works "+username;
 
-//output
-alert(welcomeMessage);
+// //output
+// alert(welcomeMessage);
+
+
+var btnTranslate = document.querySelector("#btn-translate");
+// console.log(btnTranslate)
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput)
+
+function clickHandler() {
+        console.log("Clicked!!");
+        console.log("input", txtInput.value);
+    };
+
+btnTranslate.addEventListener("click", 
+    clickHandler)
+
+
